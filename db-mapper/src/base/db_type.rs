13cbb809d20551b2b3132a0087abb261e1fec7b4
wocan23 <1115870494@qq.com>
2026-02-11 10:57:09 +0800

    泛型简化调用

--- db-mapper/src/base/db_type.rs
+++ db-mapper/src/base/db_type.rs
@@ -16,57 +16,76 @@ pub enum DbType{
     SqlServer,
 }
 
-impl PageSqlGenerator for DbType {
-    fn gen_page_query_sql(&self, query_sql: &str, current_page: u64, page_size: u64) -> (String, u64, u64) {
-        match self {
-            DbType::Mysql=>{
-                MYSQL_SQL_GENERATOR.gen_page_query_sql(query_sql, current_page, page_size)
-            }
-            DbType::Postgres=>{
-                POSTGRES_SQL_GENERATOR.gen_page_query_sql(query_sql, current_page, page_size)
-            }
-            DbType::Oracle=>{
-                ORACLE_SQL_GENERATOR.gen_page_query_sql(query_sql, current_page, page_size)
-            }
-            DbType::Sqlite=>{
-                SQLITE_SQL_GENERATOR.gen_page_query_sql(query_sql, current_page, page_size)
-            }
-            DbType::SqlServer=>{
-                SQL_SERVER_SQL_GENERATOR.gen_page_query_sql(query_sql, current_page, page_size)
+macro_rules! impl_db_method_generic {
+    ($method:ident($($param:ident: $param_type:ty),*) -> $ret:ty) => {
+        fn $method(&self, $($param: $param_type),*) -> $ret {
+            match self {
+                DbType::Mysql => MYSQL_SQL_GENERATOR.$method($($param),*),
+                DbType::Postgres => POSTGRES_SQL_GENERATOR.$method($($param),*),
+                DbType::Oracle => ORACLE_SQL_GENERATOR.$method($($param),*),
+                DbType::Sqlite => SQLITE_SQL_GENERATOR.$method($($param),*),
+                DbType::SqlServer => SQL_SERVER_SQL_GENERATOR.$method($($param),*),
             }
         }
-    }
+    };
+    // 模式1: 单个泛型参数，有 where 子句
+    ($method:ident <$g:ident> ($($param:ident: $param_type:ty),*) -> $ret:ty where $($where_clause:tt)+) => {
+        fn $method <$g> (&self, $($param: $param_type),*) -> $ret
+        where $($where_clause)+
+        {
+            match self {
+                DbType::Mysql => MYSQL_SQL_GENERATOR.$method($($param),*),
+                DbType::Postgres => POSTGRES_SQL_GENERATOR.$method($($param),*),
+                DbType::Oracle => ORACLE_SQL_GENERATOR.$method($($param),*),
+                DbType::Sqlite => SQLITE_SQL_GENERATOR.$method($($param),*),
+                DbType::SqlServer => SQL_SERVER_SQL_GENERATOR.$method($($param),*),
+            }
+        }
+    };
+
+    // 模式2: 单个泛型参数带约束，有 where 子句
+    ($method:ident <$g:ident: $bound:path> ($($param:ident: $param_type:ty),*) -> $ret:ty where $($where_clause:tt)+) => {
+        fn $method <$g: $bound> (&self, $($param: $param_type),*) -> $ret
+        where $($where_clause)+
+        {
+            match self {
+                DbType::Mysql => MYSQL_SQL_GENERATOR.$method($($param),*),
+                DbType::Postgres => POSTGRES_SQL_GENERATOR.$method($($param),*),
+                DbType::Oracle => ORACLE_SQL_GENERATOR.$method($($param),*),
+                DbType::Sqlite => SQLITE_SQL_GENERATOR.$method($($param),*),
+                DbType::SqlServer => SQL_SERVER_SQL_GENERATOR.$method($($param),*),
+            }
+        }
+    };
+
+    // 模式3: 单个泛型参数，没有 where 子句
+    ($method:ident <$g:ident> ($($param:ident: $param_type:ty),*) -> $ret:ty) => {
+        fn $method <$g> (&self, $($param: $param_type),*) -> $ret {
+            match self {
+                DbType::Mysql => MYSQL_SQL_GENERATOR.$method($($param),*),
+                DbType::Postgres => POSTGRES_SQL_GENERATOR.$method($($param),*),
+                DbType::Oracle => ORACLE_SQL_GENERATOR.$method($($param),*),
+                DbType::Sqlite => SQLITE_SQL_GENERATOR.$method($($param),*),
+                DbType::SqlServer => SQL_SERVER_SQL_GENERATOR.$method($($param),*),
+            }
+        }
+    };
 }
 
 impl WhereSqlGenerator for DbType {
 
 }
 
-impl BaseSqlGenerator for DbType{
-    fn gen_insert_and_get_id_sql<E>(&self, e:&E) -> (String, Vec<ParamValue>)
-    where
-        E: Entity
-    {
-        match self {
-            DbType::Mysql=>{
-                MYSQL_SQL_GENERATOR.gen_insert_and_get_id_sql::<E>(e)
-            }
-            DbType::Postgres=>{
-                POSTGRES_SQL_GENERATOR.gen_insert_and_get_id_sql::<E>(e)
-            }
-            DbType::Oracle=>{
-                ORACLE_SQL_GENERATOR.gen_insert_and_get_id_sql::<E>(e)
-            }
-            DbType::Sqlite=>{
-                SQLITE_SQL_GENERATOR.gen_insert_and_get_id_sql::<E>(e)
-            }
-            DbType::SqlServer=>{
-                SQL_SERVER_SQL_GENERATOR.gen_insert_and_get_id_sql::<E>(e)
-            }
-        }
-    }
-}
 
 impl QueryWrapperSqlGenerator for DbType{
 
+}
+
+impl PageSqlGenerator for DbType {
+    impl_db_method_generic!(gen_page_query_sql(query_sql: &str, current_page: u64, page_size: u64) -> (String, u64, u64));
+    impl_db_method_generic!(gen_page_total_sql(query_sql: &str) -> String);
+}
+
+impl BaseSqlGenerator for DbType {
+    impl_db_method_generic!(gen_insert_and_get_id_sql<E>(e: &E) -> (String, Vec<ParamValue>)where E: Entity);
 }

e5d6dd520ddd465d2df38544ee44fa70131eaa75
wocan23 <1115870494@qq.com>
2026-03-16 15:54:23 +0800

    修改DB_NAME_REGISTRY

--- huiyu-db-mapper-core/src/pool/datasource.rs
+++ huiyu-db-mapper-core/src/pool/datasource.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use crate::base::db_type::DbType;
 use lazy_static::lazy_static;
 use dashmap::DashMap;
@@ -5,18 +6,20 @@ use tracing::warn;
 use tokio::task_local;
 
 task_local! {
-    pub static DB_NAME_REGISTRY: String;
+    pub static DB_NAME_REGISTRY: Arc<String>;
 }
 
+// 静态默认值，只分配一次
 lazy_static! {
     static ref DB_TYPE_REGISTRY: DashMap<String, DbType> = DashMap::new();
+    static ref DEFAULT_NAME: Arc<String> = Arc::new("default".to_string());
 }
 
-pub fn get_datasource_name() -> String {
+pub fn get_datasource_name() -> Arc<String> {
     if let Some(name) = DB_NAME_REGISTRY.try_get().ok() {
         return name.clone()
     }
-    "default".to_string()
+    DEFAULT_NAME.clone()
 }
 
 pub(crate) fn set_datasource_type(name: String, data_type: DbType) {
